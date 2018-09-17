4da9dc1708c0836cb148be20fa96c8a6cd353160
lucashuet93 <lucashh@microsoft.com>
2018-09-17 11:45:20 -0400

    basic header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
+import { initializeIcons } from '@uifabric/icons';
 import * as React from 'react';
+
+initializeIcons();
 
 class Header extends React.Component {
   public render() {
     return (
-      <div className='ms-Grid-row'>
-        <div className="ms-Grid-col ms-sm1 ms-md1">Logo</div>
-        <div className="ms-Grid-col ms-sm4 ms-md4"> <span className='ms-font-su'>Durable Functions Demo</span></div>
+      <div className='ms-Grid-row ms-bgColor-themePrimary header'>
+        <div className="ms-Grid-col ms-sm1 ms-md1"><span className='ms-font-su ms-fontColor-white'><i className="ms-Icon ms-Icon--LightningBolt"/></span></div>
+        <div className="ms-Grid-col ms-sm11 ms-md11"> <span className='ms-font-su ms-fontColor-white'>Durable Functions Demo</span></div>
       </div>
     );
   }

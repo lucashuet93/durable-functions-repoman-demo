e58b6e53ea0e66f2a0cb261a7016aaad58a52332
lucashuet93 <lucashh@microsoft.com>
2018-09-19 13:38:36 -0400

    update demo title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,11 +8,11 @@ class Header extends React.Component<{}, {}> {
     return (
       <div className='ms-Grid-row ms-bgColor-themeDark header'>
         {/* Mobile */}
-        <div className="ms-Grid-col ms-sm2 ms-hiddenMdUp"><img width={30} src={require('../assets/images/functionsLogo.svg')} /></div>
-        <div className="ms-Grid-col ms-sm10 ms-hiddenMdUp header-text"><span className='ms-font-xl ms-fontColor-white'>Durable Functions Demo</span></div>
+        <div className="ms-Grid-col ms-sm1 ms-hiddenMdUp"><img width={30} src={require('../assets/images/functionsLogo.svg')} /></div>
+        <div className="ms-Grid-col ms-sm11 ms-hiddenMdUp header-text"><span className='ms-font-l ms-fontColor-white'>Durable Functions - Repo Man Demo</span></div>
         {/* Desktop */}
         <div className="ms-Grid-col ms-hiddenSm ms-md1"><img width={70} src={require('../assets/images/functionsLogo.svg')} /></div>
-        <div className="ms-Grid-col ms-hiddenSm ms-md11 header-text"><span className='ms-font-su ms-fontColor-white'>Durable Functions Demo</span></div>
+        <div className="ms-Grid-col ms-hiddenSm ms-md11 header-text"><span className='ms-font-su ms-fontColor-white'>Durable Functions - Repo Man Demo</span></div>
       </div>
     );
   }

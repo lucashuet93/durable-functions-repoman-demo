5b60fb6292e6f0a4628e4331a4960d3a06e4aa9b
lucashuet93 <lucashh@microsoft.com>
2018-09-19 16:09:45 -0400

    optimize for all screen sizes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,11 +8,11 @@ class Footer extends React.Component<{}, {}> {
     return (
       <div className='ms-Grid-row ms-bgColor-neutralLight footer'>
         {/* Mobile */}
-        <div className="ms-Grid-col ms-sm7 ms-hiddenMdUp footer-left"><span className='ms-font-s ms-fontColor-black'><a href='https://docs.microsoft.com/en-us/azure/azure-functions/durable-functions-overview'>Get Started with Durable Functions</a></span></div>
-        <div className="ms-Grid-col ms-sm5 ms-hiddenMdUp footer-right"><span className='ms-font-s ms-fontColor-black'><a href='https://github.com/lucashuet93'>Demo Source Code</a></span></div>
+        <div className="ms-Grid-col ms-sm8 ms-hiddenMdUp footer-left"><span className='ms-font-s'><a href='https://docs.microsoft.com/en-us/azure/azure-functions/durable-functions-overview'>Get Started with Durable Functions</a></span></div>
+        <div className="ms-Grid-col ms-sm4 ms-hiddenMdUp footer-right"><span className='ms-font-s'><a href='https://github.com/lucashuet93'>Source Code</a></span></div>
         {/* Desktop */}
-        <div className="ms-Grid-col ms-hiddenSm ms-md6 footer-left"><span className='ms-font-l ms-fontColor-black'><a href='https://docs.microsoft.com/en-us/azure/azure-functions/durable-functions-overview'>Get Started with Durable Functions</a></span></div>
-        <div className="ms-Grid-col ms-hiddenSm ms-md6 footer-right"><span className='ms-font-l ms-fontColor-black'><a href='https://github.com/lucashuet93'>Demo Source Code</a></span></div>
+        <div className="ms-Grid-col ms-hiddenSm ms-md6 footer-left"><span className='ms-font-m-plus'><a href='https://docs.microsoft.com/en-us/azure/azure-functions/durable-functions-overview'>Get Started with Durable Functions</a></span></div>
+        <div className="ms-Grid-col ms-hiddenSm ms-md6 footer-right"><span className='ms-font-m-plus'><a href='https://github.com/lucashuet93/durable-functions-repoman-demo'>Source Code</a></span></div>
       </div>
     );
   }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,9 +10,15 @@ class Header extends React.Component<{}, {}> {
         {/* Mobile */}
         <div className="ms-Grid-col ms-sm1 ms-hiddenMdUp"><img width={30} src={require('../assets/images/functionsLogo.svg')} /></div>
         <div className="ms-Grid-col ms-sm11 ms-hiddenMdUp header-text"><span className='ms-font-l ms-fontColor-white'>Durable Functions - Repo Man Demo</span></div>
-        {/* Desktop */}
-        <div className="ms-Grid-col ms-hiddenSm ms-md1"><img width={70} src={require('../assets/images/functionsLogo.svg')} /></div>
-        <div className="ms-Grid-col ms-hiddenSm ms-md11 header-text"><span className='ms-font-su ms-fontColor-white'>Durable Functions - Repo Man Demo</span></div>
+        {/* Desktop - Medium */}
+        <div className="ms-Grid-col ms-hiddenSm ms-md1 ms-hiddenLgUp"><img width={50} src={require('../assets/images/functionsLogo.svg')} /></div>
+        <div className="ms-Grid-col ms-hiddenSm ms-md11 ms-hiddenLgUp header-text"><span className='ms-font-xl ms-fontColor-white'>Durable Functions - Repo Man Demo</span></div>
+        {/* Desktop - Large */}
+        <div className="ms-Grid-col ms-hiddenMdDown ms-lg1 ms-hiddenXlUp"><img width={60} src={require('../assets/images/functionsLogo.svg')} /></div>
+        <div className="ms-Grid-col ms-hiddenMdDown ms-lg11 ms-hiddenXlUp header-text"><span className='ms-font-xxl ms-fontColor-white'>Durable Functions - Repo Man Demo</span></div>
+        {/* Desktop - X Large */}
+        <div className="ms-Grid-col ms-hiddenLgDown ms-xl1"><img width={70} src={require('../assets/images/functionsLogo.svg')} /></div>
+        <div className="ms-Grid-col ms-hiddenLgDown ms-xl11 header-text"><span className='ms-font-su ms-fontColor-white'>Durable Functions - Repo Man Demo</span></div>
       </div>
     );
   }
